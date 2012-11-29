92f68bec2b4ae23da79972019db43d58692b7107
kalidasya <oliolart@gmail.com>
2012-11-29 11:06:15 +0100

    SONARPLUGINS-2474, SONARPLUGINS-2483 fix failing tests

--- erlang-checks/src/test/java/org/sonar/erlang/checks/CommentRegularExpressionCheckTest.java
+++ erlang-checks/src/test/java/org/sonar/erlang/checks/CommentRegularExpressionCheckTest.java
@@ -2,17 +2,17 @@
  * Sonar Erlang Plugin
  * Copyright (C) 2012 Tamas Kende
  * kende.tamas@gmail.com
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
@@ -47,7 +47,7 @@ public class CommentRegularExpressionCheckTest {
     SourceFile file = ErlangAstScanner.scanSingleFile(new File(
         "src/test/resources/checks/commentcheck.erl"), check);
     CheckMessagesVerifier.verify(file.getCheckMessages()).next().atLine(2).withMessage(
-        "The regular expression matches this comment").next().atLine(9);
+        "The violation message").next().atLine(9);
   }
 
   @Test
