e8e1318f0ab1b53aa90cff5790a1976b9cb2f187
Linda Martin <linda.martin@sonarsource.com>
2014-04-29 09:04:39 +0200

    Fix quality flaws.

--- sonar-erlang-plugin/src/main/java/org/sonar/plugins/erlang/dialyzer/ErlangXmlRuleParser.java
+++ sonar-erlang-plugin/src/main/java/org/sonar/plugins/erlang/dialyzer/ErlangXmlRuleParser.java
@@ -2,17 +2,17 @@
  * Sonar Erlang Plugin
  * Copyright (C) 2012 Tamas Kende
  * kende.tamas@gmail.com
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
@@ -26,25 +26,21 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
 
 public final class ErlangXmlRuleParser implements ServerComponent {
 
   /**
    * Warning : the input stream is closed in this method
-   * 
+   *
    * @throws IOException
    * @throws SAXException
    * @throws ParserConfigurationException
-   * 
    * @throws DocumentException
    */
   public List<ErlangRule> parse(InputStream input) {
-    List<ErlangRule> rules = new ArrayList<ErlangRule>();
     SAXParserFactory factory = SAXParserFactory.newInstance();
     SAXParser saxParser;
     RuleHandler a = new RuleHandler();
@@ -56,8 +52,6 @@ public final class ErlangXmlRuleParser implements ServerComponent {
     } catch (IOException e) {
     }
 
-    rules = a.getRules();
-    return rules;
-
+    return a.getRules();
   }
 }
