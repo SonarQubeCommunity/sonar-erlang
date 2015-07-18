d9de822b0b3afac4fbcd8503186f3a3a1826430e
Nathaniel Waisbrot <code@waisbrot.net>
2015-07-17 23:46:35 -0400

    Disable the ErlangLibrarySensor plugin
    Disables the ErlangLibrarySensor plugin, which is reporting
    a `javax.persistence.PersistenceException` when run with
    SonarQube 5.1

--- sonar-erlang-plugin/src/test/java/org/sonar/plugins/erlang/ErlangPluginTest.java
+++ sonar-erlang-plugin/src/test/java/org/sonar/plugins/erlang/ErlangPluginTest.java
@@ -35,7 +35,7 @@ public class ErlangPluginTest {
 
   @Test
   public void testGetExtensions() throws Exception {
-    assertThat(plugin.getExtensions().size()).isEqualTo(13);
+    assertThat(plugin.getExtensions().size()).isEqualTo(12);
   }
 
 }
