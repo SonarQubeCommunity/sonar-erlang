258239f831bd6070e99d14e99b11fbf086fa92e9
Denes Hegedus <hegedenes@cursorinsight.com>
2018-03-27 13:44:47 +0200

    Hide maven 'Downloading/Downloaded' messages

--- travis.sh
+++ travis.sh
@@ -17,14 +17,16 @@ CI)
 IT)
   installTravisTools
 
-  mvn package -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
+  mvn package -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true \
+    -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
 
   if [ "$SQ_VERSION" == "DEV" ]; then
     build_snapshot "SonarSource/sonarqube"
   fi
 
   cd its/plugin
-  mvn -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false install
+  mvn -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false \
+    -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn install
   ;;
 
 *)
