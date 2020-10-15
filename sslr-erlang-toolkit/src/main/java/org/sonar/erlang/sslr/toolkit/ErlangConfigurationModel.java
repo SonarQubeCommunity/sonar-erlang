13442578b7a1f6737dd40b7e2210dbab85901ed6
Andris Raugulis <moo@arthepsy.eu>
2020-10-15 20:04:06 +0300

    Fix fb-contrib:LO_APPENDED_STRING_IN_FORMAT_STRING.

--- sslr-erlang-toolkit/src/main/java/org/sonar/erlang/sslr/toolkit/ErlangConfigurationModel.java
+++ sslr-erlang-toolkit/src/main/java/org/sonar/erlang/sslr/toolkit/ErlangConfigurationModel.java
@@ -89,10 +89,10 @@ public class ErlangConfigurationModel extends AbstractConfigurationModel {
     String propertyValue = System.getProperty(propertyKey);
 
     if (propertyValue == null) {
-      LOG.info("The property \"" + propertyKey + "\" is not set, using the default value \"" + defaultValue + "\".");
+      LOG.info("The property \"{}\" is not set, using the default value \"{}}\".", propertyKey,  defaultValue);
       return defaultValue;
     } else {
-      LOG.info("The property \"" + propertyKey + "\" is set, using its value \"" + propertyValue + "\".");
+      LOG.info("The property \"{}\" is set, using its value \"{}\".", propertyKey, propertyValue);
       return propertyValue;
     }
   }
