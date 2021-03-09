dc1b1c7f2d9d71ffacb09b2b67d06eb0f9bfc8de
Daniils Petrovs <dpetrovs@evolution.com>
2021-03-09 10:09:10 +0200

    CVI-11181 Update license headers

--- sslr-erlang-toolkit/src/test/java/org/sonar/erlang/sslr/toolkit/ErlangConfigurationModelTest.java
+++ sslr-erlang-toolkit/src/test/java/org/sonar/erlang/sslr/toolkit/ErlangConfigurationModelTest.java
@@ -1,3 +1,23 @@
+/*
+ * SonarQube Erlang Plugin
+ * Copyright © 2012-2018 Tamas Kende <kende.tamas@gmail.com>
+ * Copyright © 2018 Denes Hegedus (Cursor Insight Ltd.) <hegedenes@cursorinsight.com>
+ * Copyright © 2020 Andris Raugulis <moo@arthepsy.eu>
+ * Copyright © 2021 Daniils Petrovs <dpetrovs@evolution.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.sonar.erlang.sslr.toolkit;
 
 import org.junit.After;
