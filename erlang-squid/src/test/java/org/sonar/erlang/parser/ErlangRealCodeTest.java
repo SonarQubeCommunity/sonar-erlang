2234f37e1cf671bbad5bd38223a644dee14c281f
Bence Golda <b@blorb.io>
2023-07-10 17:00:26 +0200

    Add map comprehension support
    Map comprehensions was introduced in Erlang 26:
    
    https://www.erlang.org/blog/otp-26-highlights/#map-comprehensions

--- erlang-squid/src/test/java/org/sonar/erlang/parser/ErlangRealCodeTest.java
+++ erlang-squid/src/test/java/org/sonar/erlang/parser/ErlangRealCodeTest.java
@@ -65,6 +65,11 @@ public class ErlangRealCodeTest {
     assertThat(b.getRootRule()).matches((readFromFile("macro_test.erl")));
   }
 
+  @Test
+  public void parseMapComprehension() throws IOException, URISyntaxException {
+    assertThat(b.getRootRule()).matches((readFromFile("map_comprehension.erl")));
+  }
+
   private String readFromFile(String fileName) throws IOException, URISyntaxException {
     StringBuilder text = new StringBuilder();
     File file = new File(ErlangRealCodeTest.class.getClassLoader().getResource(fileName)
